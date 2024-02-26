d7a16799bbf091652e3936f664e69216102ab3df
Dylan Hearn <dylanhearn123@gmail.com>
2024-02-26 04:17:45 +0000

    update: project live link

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -36,7 +36,7 @@ const Projects = () => {
         <ProjectCard
           title="Home Chef"
           description="Using Wordpress, I created a custom theme featuring advanced custom fields (ACF's) and custom posts. The site showcases meals, ingredients delivered, intended to be cooked at home. The ACF's allow the meals, ingredients and difficulty to be rendered dynamically."
-          liveLink="https://hello-chef-e67f16.ingress-haven.ewp.live/"
+          liveLink="https://hellochef.org/"
           codeLink="https://github.com/DylanHearn1/WP-Menu-Theme"
           image={chef}
           gif={chefgif}
